38a87ea1bafaf0ada4d6d7273152474b82801df1
sarath <sarathdon9245@gmail.com>
2020-08-17 12:13:45 +0530

    Updates

---
+++ precache-manifest.02ac92ee06b046143d9982be174f60f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5888efc2091bea125213f08aa9f085",
+    "url": "/Basic-react-todo-app/index.html"
+  },
+  {
+    "revision": "b86973f26ca023d4b57b",
+    "url": "/Basic-react-todo-app/static/css/main.96cbc6f3.chunk.css"
+  },
+  {
+    "revision": "a2192fd25ddc6c1c48c8",
+    "url": "/Basic-react-todo-app/static/js/2.edf75550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basic-react-todo-app/static/js/2.edf75550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86973f26ca023d4b57b",
+    "url": "/Basic-react-todo-app/static/js/main.7763fd63.chunk.js"
+  },
+  {
+    "revision": "e634b17b5ddcd98205d6",
+    "url": "/Basic-react-todo-app/static/js/runtime-main.127995a9.js"
+  }
+]);
